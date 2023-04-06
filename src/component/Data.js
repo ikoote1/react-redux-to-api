478a661200c80c05aca439511af3193452594759
ikoote1 <ikootepreim@gmail.com>
2023-04-06 11:21:27 +0300

    Fetched data

--- src/component/Data.js
+++ src/component/Data.js
@@ -1,19 +1,36 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getUsers } from "../store/users/usersSlice";
+import { useEffect } from "react";
 
 const Data = () => {
-    const {users} = useSelector((state) => state.user)
-    return(
-        <div>
-            {users.map((user) => (
-                <div key={user.id}>
-                    <h3>{user.firstName}</h3>
-                    {' '}
-                    <h3>{user.lastName}</h3>
-                </div>
-            ))}
-        </div>
-    )
+    const {users, isLoading, error} = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getUsers(123))
+    },[]);
+    if(isLoading){
+        return(
+            <h2>Leading......</h2>
+        )
+    } else if (error) {
+        return (
+            <div>Error</div>
+        )
+    } else {
+        return(
+            <div>
+                {users.map((user) => (
+                    <div className="names" key={user.login.uuid}>
+                        <h3>{user.name.first}</h3>
+                        {' '}
+                        <h3>{user.name.last}</h3>
+                    </div>
+                ))}
+            </div>
+        )
+    }
 }
 
 export default Data
--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,14 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+
+const url = 'https://randomuser.me/api/?results=5';
+
+export const getUsers = createAsyncThunk('user/getUsers', async (action, thunkAPI) => {
+    try{
+        const resp = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'content-type': 'application.json'
+            }
+        });
+        const data = await resp.json();
+        console.log(data);
+        return data
+    } catch (error) {
+        return thunkAPI.rejectWithValue('Thunk Error')
+    }
+})
 
 const initialState= {
     users: [],
     isLoading: true,
-    error: null,
+    error: undefined,
 }
 
 const usersSlice = createSlice({
     name: 'user',
     initialState,
+    extraReducers: (builder) => {
+        builder
+          .addCase(getUsers.pending, (state) => {
+            state.isLoading = true;
+          })
+          .addCase(getUsers.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.users = action.payload.results;
+          })
+          .addCase(getUsers.rejected, (state) => {
+            state.isLoading = false;
+            state.error = 'State Error'
+          })
+      }
 });
 
 export default usersSlice.reducer;
